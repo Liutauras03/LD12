921010820dbd7b6c0dc53ead0c8b415e5d5dee23
Liutauras03 <152449044+Liutauras03@users.noreply.github.com>
2023-12-08 04:33:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 function submitForm() {
-    // Gauname įvestus duomenis
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
-    var eventDate = new Date(document.getElementById('eventDate').value);
+    var eventDateInput = document.getElementById('eventDate');
+    
+    
+    if (!firstName || !lastName || !eventDateInput.value) {
+      alert('Visi laukai privalo būti užpildyti.');
+      return;
+    }
   
-    // Apskaičiuojame dienas iki įvykio
+    var eventDate = new Date(eventDateInput.value);
+    if (isNaN(eventDate.getTime())) {
+      alert('Neteisingas datos formatas.');
+      return;
+    }
+  
     var currentDate = new Date();
+    if (eventDate <= currentDate) {
+      alert('Data turi būti ateityje.');
+      return;
+    }
+  
     var daysUntilEvent = Math.ceil((eventDate - currentDate) / (1000 * 60 * 60 * 24));
   
-    // Pasirinkime tinkamą dienos formą
+  
     var dayWordForm = (daysUntilEvent === 1) ? 'diena' : 'dienos';
   
-    // Išvedame rezultatą į konsolę
+
     console.log(firstName + ' ' + lastName + ' nugyveno ' + daysUntilEvent + ' ' + dayWordForm + '.');
-    
-    // Išvedame rezultatą į HTML puslapį
+  
+
     var resultContainer = document.createElement('div');
     resultContainer.innerHTML = '<p>Vartotojo vardas: ' + firstName + '</p>' +
                                 '<p>Pavardė: ' + lastName + '</p>' +
                                 '<p>Laukiamo įvykio data: ' + eventDate.toDateString() + '</p>' +
-                                '<p>Nugyventi dienų: ' + daysUntilEvent + ' ' + dayWordForm + '</p>';
+                                '<p>Nugyventa dienų: ' + daysUntilEvent + ' ' + dayWordForm + '</p>';
   
     document.body.appendChild(resultContainer);
+  
+
+    if (daysUntilEvent <= 1000) {
+      resultContainer.style.fontSize = '10px';
+    } else if (daysUntilEvent <= 7000) {
+      resultContainer.style.fontSize = '16px';
+    } else {
+      resultContainer.style.fontSize = '20px';
+    }
   }
   
